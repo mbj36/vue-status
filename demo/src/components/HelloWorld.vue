c8c446c7b5b94e6bd0cee99f677b6f89f594ee3e
mbj36 <mohitbajo36@gmail.com>
2018-05-23 20:39:07 +0530

    Adding demo

---
+++ demo/src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <vue-status>
+    <h2 slot="online">You are online</h2>
+    <h2 slot="offline">You are offline</h2>
+  </vue-status>
+</template>
+
+<script>
+  import VueStatus from 'vue-status';
+  export default {
+    components: {
+      VueStatus
+    }
+  };
+</script>
